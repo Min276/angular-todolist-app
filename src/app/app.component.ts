21ca0a92855f60f5822809d2cf5156da2b0403b8
Min Khant Kyaw <minkhantkyaw@globalwave.company>
2023-07-24 12:04:48 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'To Do List';
+  tasks: Task[] = [
+    new Task("Visit Ann"),
+    new Task("Call Dad"),
+    new Task("Go to the gym"),
+    new Task("Wash the dishes"),
+    new Task("Shop for the party")
+  ]
+
+  @ViewChild('newTask') inputName: any; // accessing the reference element
+
+
+  add(newTask: string) {
+    if (newTask.trim().length > 0) {
+      this.tasks.push(new Task(newTask))
+      this.inputName.nativeElement.value = ""
+    }
+  }
+
+  remove(existingTask: Task) {
+    let userConfirmed = confirm(`Are you sure you want to remove "${existingTask.title}" ?`)
+
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask)
+    }
+  }
+
+}
+
+
+class Task {
+
+  constructor(public title: string) {
+
+  }
+
+  toggleIsDone() {
+    this.isDone = !this.isDone;
+  }
+
+  public isDone = false;
+
 }
